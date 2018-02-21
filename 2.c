3a7053340790fc920e38dc654664342bacbc0b72
betelgeuse06 <mintblue2006@naver.com>
2018-02-21 12:44:33 +0900

    2~3 input add

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int num1, num2;
+  scanf("%d %d\n", num1, num2);
+  int num3 = num1 + num2;
+  printf("%d\n", num3);
+  return 0;
+}
---
+++ 3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int num1, num2, num4;
+  scanf("%d %d %d\n", num1, num2, num4);
+  int num3 = num1 + num2 + num4;
+  printf("%d\n", num3);
+  return 0;
+}
